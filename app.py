2946811e8fe3d533566aa60f5d6004714a776b3e
dongphuongai <chamcongdongphuong@gmail.com>
2025-07-24 10:46:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
-import pandas as pd
-
-# Lấy secrets từ streamlit cloud
-secrets = st.secrets["gcp_service_account"]
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-credentials = Credentials.from_service_account_info(secrets, scopes=scopes)
-
+credentials = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"], scopes=scopes
+)
 client = gspread.authorize(credentials)
 
-# Mở Google Sheet bằng TÊN (hoặc dùng .open_by_key nếu có ID)
-spreadsheet = client.open("QuanLyVatTu")
-worksheet = spreadsheet.worksheet("XuatNhapTon")
-
-# Lấy dữ liệu
+# Ví dụ đọc dữ liệu
+sheet_id = "1QNOHfJw3kRAC5BSfb0YsEfYzhWDYdEFi0L4Rk1t141A"
+worksheet = client.open_by_key(sheet_id).worksheet("XuatNhapTon")
 data = worksheet.get_all_records()
-df = pd.DataFrame(data)
 
-st.title("📦 Quản lý vật tư")
-st.dataframe(df)
+st.title("📦 Quản lý Xuất Nhập Tồn Vật Tư")
+st.write(data)
